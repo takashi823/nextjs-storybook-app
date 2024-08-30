863f264b0e1eb6df66ceb3cb4433359e6c73120d
takashi823 <uchimura.takashi724@gmail.com>
2024-08-30 16:25:38 +0900

    fix: cssの設定修正

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,6 @@
 import type { Preview } from "@storybook/react";
+// CSSの設定を読み込むために設定
+import "../src/app/globals.css"
 
 const preview: Preview = {
   parameters: {
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,8 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    // storybook追加時に作成されたフォルダにtailwind cssを適用する
+    "./src/stories/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
